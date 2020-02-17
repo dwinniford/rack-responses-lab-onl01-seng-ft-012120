840e902f9270939d0c94e90477cdc835a590775d
Dave Winniford <dwinniford@gmail.com>
2020-02-17 15:25:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
 def call(time)
   resp = Rack::Response.new 
-  if time  < 12 
+  if time  < Time.new(2015, 11, 27, 12, 00)
     resp.write "Good Morning!"
   else 
     resp.write "Good Afternoon!"
