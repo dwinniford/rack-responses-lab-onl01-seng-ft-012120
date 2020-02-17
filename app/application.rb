739dd90918f6cfad48ab7c9f3901adb4449883ed
Dave Winniford <dwinniford@gmail.com>
2020-02-17 15:20:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,10 @@ class Application
 def call
   resp = Rack::Response.new 
   if Time.now < 12 
+    resp.write "Good Morning!"
+  else 
+    resp.write "Good Afternoon!"
+  end 
 end 
 
 def greeting 
